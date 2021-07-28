5bf9f93c99d8f17bb2037f967fa19438bab560f3
orl1k <orlov.dvi@gazprom-neft.ru>
2021-07-29 02:40:05 +0300

    Update
    Рефакторинг папок
    Отделены celery worker'ы и API
    Добавлен prod docker-compose без volume'ов

--- worker/worker.py
+++ worker/worker.py
@@ -4,7 +4,12 @@ from datetime import datetime
 from celery import Celery
 
 # celery = Celery(__name__, broker="amqp://rabbitmq:5672", backend="redis://redis:6379/0")
-celery = Celery(__name__, broker=os.environ['CELERY_BROKER_URL'], backend=os.environ['CELERY_RESULT_BACKEND'])
+celery = Celery(
+    __name__,
+    broker=f"amqp://{os.getenv('RABBITMQ_USER')}:{os.getenv('RABBITMQ_PASS')}@rabbitmq:{os.getenv('RABBITMQ_PORT')}//",
+    backend=f"redis://:{os.getenv('REDIS_PASSWORD')}@redis:{os.getenv('REDIS_PORT_NUMBER')}/0",
+)
+
 celery.conf.update(task_track_started=True)
 
 
