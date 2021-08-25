da7795dff0d41c96102bfc421a3b7cd75dc2d089
orl1k <orlov.dvi@gazprom-neft.ru>
2021-08-25 18:39:43 +0300

    Добавлено разделение форм на разные эндпоинты

--- app/main.py
+++ app/main.py
@@ -84,6 +84,51 @@ async def handle_args(
     return task_item
 
 
+@app.post("/weather_script", status_code=201, response_model=TaskItem)
+async def handle_weather_args(
+    request: Request,
+    args: models.ScriptArgs = Depends(models.WeatherScriptArgs.as_form),
+) -> TaskItem:
+    """
+    Выберите аргументы скрипта сборки датасета:
+    - **dataset_date**: дата, за которую будет собран датасет
+    - **rasters_path**: директория, содержащая снимки
+    - **datasets_path**: директория с датасетами
+    - **icemaps_path**: директория с датасетами
+    - **land_path**: путь к шейпу с землей
+    - **age, concentrat, age_group**: характеристики льда, которые будут добавлены в датасет
+    - **simple**: добавляемые текстурные характеристики из группы simple
+    - **advanced**: добавляемые текстурные характеристики из группы advanced
+    """
+    try:
+        args_dict = args.dict()
+        task = celery_app.send_task(
+            "run_script",
+            kwargs=args_dict,
+            priority=args.task_priority,
+        )
+    except Exception:
+        message = "Ошибка при отправке задачи"
+        print(message)
+        print(traceback.format_exc())
+        raise HTTPException(status_code=500, detail=message)
+
+    try:
+        task_item = TaskItem(id=task.id, kwargs=args_dict)
+        task_queue_web.appendleft(task_item)
+    except Exception:
+        message = "Ошибка очереди веб-интерфейса"
+        print(message)
+        print(traceback.format_exc())
+        raise HTTPException(status_code=500, detail=message)
+
+    print("-" * 100)
+    print(task_item)
+    print("-" * 100)
+
+    return task_item
+
+
 @app.get("/flower", status_code=301)
 def flower_redirect(
     request: Request, settings: config.Settings = Depends(get_settings)
